11d0aa466e70bca79887ce36ce0c504bbb655bf7
Lars <larsmackenzie@Larss-MacBook-Pro.local>
2018-08-13 13:26:48 -0500

    completed base mode

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//REQUIRES & CONSTS
 const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser');
@@ -5,13 +6,13 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/taskpig';
 const Schema = mongoose.Schema; 
-
+//mongo schema 
 const TaskSchema = new Schema({
     task: {type: String}, 
     category: {type: String},
     complete: {type: Boolean}
 });
-
+//mongo connections 
 mongoose.connect(mongoURI, {useNewUrlParser: true});
 mongoose.connection.on('open', ()=>{
     console.log('Connected to Mongo');
@@ -19,9 +20,10 @@ mongoose.connection.on('open', ()=>{
 mongoose.connection.on('error', (error) =>{
     console.log('Connection to Mongo Failed. Error:', error);
 });
-
+//mongo model 
 const Tasks = mongoose.model('tasks', TaskSchema);
 
+//USES
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); 
 app.use(express.static('server/public'));
